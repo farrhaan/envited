f5ab433f45b443779b6f5aff80a2b5c6aa648aec
M Farhan <m.ffarrhan@gmail.com>
2022-10-04 04:45:52 +0500

    feat: homepage responsive laptop

--- Components/HomePage/index.jsx
+++ Components/HomePage/index.jsx
@@ -3,20 +3,30 @@ import React from "react";
 import Button from "../Button";
 export default function MainPage() {
   return (
-    <div className='main-page'>
-      <p className='heading-text'>
-        Imagine if<span className='heading-gradient heading-text'><br/>Snapchat<br/></span>had
-        events.
-      </p>
-      <p className='main-text'>
-        Easily host and share events with your friends across any social media.
-      </p>
-      <img src='/svgs/Landing-page-image.svg' alt='' className='main-image' />
-      <Link href='/create'>
-        <a>
-          <Button text="🎉 Create my event"/>
-        </a>
-      </Link>
+    <div className="home-wrap">
+      <img src='/svgs/Landing-page-image.svg' alt='' className='main-image-large' />
+      
+      <div className='main-page'>
+        <p className='heading-text'>
+          Imagine if
+          <span className='heading-gradient heading-text'>
+            <br />
+            Snapchat
+            <br />
+          </span>
+          had events.
+        </p>
+        <p className='main-text'>
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+        <img src='/svgs/Landing-page-image.svg' alt='' className='main-image' />
+        <Link href='/create'>
+          <a>
+            <Button text='🎉 Create my event' />
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
